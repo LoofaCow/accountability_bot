b1b67d30c1d6745255228fc2e5ce5b2805487bea
Loofacow <tj98129@gmail.com>
2025-02-22 11:52:22 -0600

    Create activate.bat
    added an environment activation simply for my lazy ass

---
+++ activate.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Activate your Python virtual environment
+call C:\Users\tj981\Documents\accountability_bot\env\Scripts\activate.bat
+REM Do any other tasks you need
+echo Environment activated!
+cmd /k
